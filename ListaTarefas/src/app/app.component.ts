69f30eb9e3bf664f5a50168b5c5b3e307318916a
Danilo Silva <danilo.silva@msn.com>
2023-01-24 20:16:16 -0300

    Adicionando uma Tarefa

--- ListaTarefas/src/app/app.component.ts
+++ ListaTarefas/src/app/app.component.ts
@@ -20,17 +20,24 @@ export class AppComponent {
         Validators.required
       ])]
     });
-
-    this.listaTarefas.push(new Tarefa(1, 'Passear com o cachorro', false));
-    this.listaTarefas.push(new Tarefa(2, 'Ir ao supermercado', false));
-    this.listaTarefas.push(new Tarefa(3, 'Cortar o cabelo', true));
   }
 
-  excluirTarefa(tarefa: Tarefa) {
+  adicionar() {
+    const id = this.listaTarefas.length + 1;
+    const titulo = this.form.controls['titulo'].value;
+    this.listaTarefas.push(new Tarefa(id, titulo, false));
+    this.limpar();
+  }
+
+  excluir(tarefa: Tarefa) {
     const index = this.listaTarefas.indexOf(tarefa);
     if (index !== -1) {
       this.listaTarefas.splice(index, 1);
     }
+  }
+
+  limpar() {
+    this.form.reset();
   }
 
   marcarComoConcluido(tarefa: Tarefa) {
