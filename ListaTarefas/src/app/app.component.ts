66e39213f6bebd4aea8c7b34da2808f9b6e16251
Danilo Silva <danilo.silva@msn.com>
2023-01-24 19:13:26 -0300

    Variáveis e Tipos

--- ListaTarefas/src/app/app.component.ts
+++ ListaTarefas/src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public listaTarefas: any[] = [];
+
+  constructor() {
+    this.listaTarefas.push('Passear com o cachorro');
+    this.listaTarefas.push('Ir ao supermercado');
+    this.listaTarefas.push('Cortar o cabelo');
+  }
 }
