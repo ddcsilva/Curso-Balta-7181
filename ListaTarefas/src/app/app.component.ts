ae024eae53d5a4c07058ed4b9355b0393fcb7980
Danilo Silva <danilo.silva@msn.com>
2023-01-24 20:00:22 -0300

    Forms Básico

--- ListaTarefas/src/app/app.component.ts
+++ ListaTarefas/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Tarefa } from 'src/models/tarefa.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,17 @@ import { Tarefa } from 'src/models/tarefa.model';
 export class AppComponent {
   public listaTarefas: Tarefa[] = [];
   public titulo: String = "Minhas tarefas";
+  public form: FormGroup = new FormGroup('');
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      titulo: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
     this.listaTarefas.push(new Tarefa(1, 'Passear com o cachorro', false));
     this.listaTarefas.push(new Tarefa(2, 'Ir ao supermercado', false));
     this.listaTarefas.push(new Tarefa(3, 'Cortar o cabelo', true));
